dbd1e1c322351cc3ecc451d497b963937ec59664
Matt Canty <matthewcanty@gmail.com>
2021-11-03 13:51:45 +0000

    add something which changes

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -3,6 +3,12 @@ variable "message" {
   default = "Hello, Production World!"
 }
 
+resource "null_resource" "null" {
+  triggers = {
+    value = timestamp()
+  }
+}
+
 output "hello_world_message" {
   value = var.message
 }
