1408ed7499d30b350bf27b3bbb9e3d50e792f534
Matt Canty <matthewcanty@gmail.com>
2021-11-03 13:10:53 +0000

    use environment templates

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -3,23 +3,6 @@ variable "message" {
   default = "Hello, Production World!"
 }
 
-variable "environment" {
-  type    = string
-  default = "production"
-}
-
 output "hello_world_message" {
   value = var.message
-}
-
-terraform {
-  required_version = "1.0.10"
-
-  backend "remote" {
-    organization = "Cantonite"
-
-    workspaces {
-      name = "terraform-deployment-pipeline-${var.environment}"
-    }
-  }
 }
