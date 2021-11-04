b457043ca68181a4689a3fc2dae3991b4d2d9d9b
Matt Canty <matthewcanty@gmail.com>
2021-11-04 15:42:40 +0000

    add back remote terraform block

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,11 +1,11 @@
 terraform {
-  # required_version = "1.0.10"
+  required_version = "1.0.10"
 
-  # backend "remote" {
-  #   organization = "Cantonite"
+  backend "remote" {
+    organization = "Cantonite"
 
-  #   workspaces {
-  #     prefix = "terraform-deployment-pipeline-"
-  #   }
-  # }
+    workspaces {
+      prefix = "terraform-deployment-pipeline-"
+    }
+  }
 }
